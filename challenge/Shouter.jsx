5a97be12f3b9602e98614b0c13bc7c05101d253e
Georgia E. Whitney <81175151+rockyrelay@users.noreply.github.com>
2023-02-12 18:57:59 +0000

    added notes around component logic and working

--- challenge/Shouter.jsx
+++ challenge/Shouter.jsx
@@ -1,20 +1,20 @@
 import { useState } from "react";
 
 function Shouter() {
-  // useState hook used to create a state variable [input] and function [setinput] to update value
-  const [input, setInput] = useState("");
+  // useState hook used to create a state variable (input) and func (setInput) to update value
+  const [inputValue, setInputValue] = useState("");
   // event handler function is defined to handle onChange event of input element
   const handleChange = (e) => {
     // func sets new value using 'setinput'
-    setInput(e.target.value);
+    setInputValue(e.target.value);
   };
   // component then renders <input> and <output> elements with input value displayed in both
   return (
     // react.fragment groups a list of children without adding extra nodes ot the DOM
     // can be shortened to <>...</>
     <>
-      <input value={input} onChange={handleChange} />
-      <output>{input.toUpperCase()}</output>
+      <input value={inputValue} onChange={handleChange} />
+      <output>{inputValue.toUpperCase()}</output>
     </>
   )
 }
