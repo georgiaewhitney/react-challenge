ca20dc25c8af5116f03fb986b1586825a246ca60
Georgia E. Whitney <81175151+rockyrelay@users.noreply.github.com>
2023-02-12 19:32:29 +0000

    challenge 3 - passing in practice, fault with test

--- challenge/App.jsx
+++ challenge/App.jsx
@@ -1,10 +1,12 @@
 import Greeting from "./Greeting.jsx";
 import Shouter from "./Shouter.jsx";
+import MouseTracker from "./MouseTracker.jsx";
 
 function App() {
   return <main>
     <Greeting name="motherfucker" />
     <Shouter />
+    <MouseTracker />
   </main>;
 }
 
---
+++ challenge/MouseTracker.jsx
@@ -0,0 +1,55 @@
+import {useState, useEffect} from "react";
+
+/*
+
+// THIS doesn't pass test because of body element created but DOES work
+
+function MouseTracker() {
+  // useState hook creates state variable and func to update value of state variable
+  // initialise state w/ array that has 0 values for x, y
+  const [coordinatesValue, setCoordinatesValue] = useState([0, 0]);
+
+  // useEffect hook to add the mousemove event listener to window, and remove when component unmounted
+  useEffect(() => {
+    const mouseTrackHandler = (e) => {
+      // updates coordinates state with current mouse coordinates when mouse moves
+      setCoordinatesValue({x: e.clientX, y: e.clientY});
+    };
+    window.addEventListener('mousemove', mouseTrackHandler);
+    return () => {
+      window.removeEventListener('mousemove', mouseTrackHandler);
+    // empty array to clean up Effect so is unmounted
+  };
+  }, []);
+  // component renders <ouput> element displaying clientX and clientY values
+  return (
+    <output>{coordinatesValue.x}, {coordinatesValue.y}</output>
+  );
+}
+*/
+
+function MouseTracker() {
+  // useState hook creates state variable and func to update value of state variable
+   // initialise state with array of 0s for x, y
+  const [coordinates, setCoordinates] = useState([0, 0]);
+
+  // useEffect hook to add mousemove event listener to window
+  useEffect(() => {
+    // show coordinates by grabbing x, y and passing through setValue func
+    function showCoordinates({clientX, clientY}) {
+      setCoordinates([clientX, clientY]);
+    }
+    // listens for mouse move and passes func to show coordinates 
+    window.addEventListener("mousemove", showCoordinates);
+    // clean up Effect to unmount it
+    return () => window.removeEventListener("mousemove", showCoordinates);
+  }, []);
+  // match x, y to state value of coordinates
+  const [xCoordinate, yCoordinate] = coordinates;
+  // render <ouput> component to display values as they update
+  return (
+    <output>{xCoordinate}, {yCoordinate}</output>
+  );
+}
+
+export default MouseTracker;
